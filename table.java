f6ea26d71aae01b815f92b00751ecbb432cd9d87
Laxman kohar verma <lkverma5422@gmail.com>
2021-06-02 15:32:04 +0530

    table.java is added

---
+++ table.java
@@ -0,0 +1,8 @@
+public class table {
+    public static void main(String[] args) {
+        int a = 2;
+        for(int i = 1; i<=10; i++){
+            System.out.println(a+" x "+i+ " = "+(a*i));
+        }
+    }
+}
